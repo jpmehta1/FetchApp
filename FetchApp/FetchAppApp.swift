ea20f491a58eaf3fb47186e70ca0b826ce0bddf6
Jeet Paresh Mehta <jpmehta@wisc.edu>
2024-10-13 12:15:27 -0500

    Initial Commit

---
+++ FetchApp/FetchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAppApp.swift
+//  FetchApp
+//
+//  Created by Jeet P Mehta on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchAppTests/FetchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAppTests.swift
+//  FetchAppTests
+//
+//  Created by Jeet P Mehta on 13/10/24.
+//
+
+import Testing
+@testable import FetchApp
+
+struct FetchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
